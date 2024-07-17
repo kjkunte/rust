a9757108e2c740713bff35f2c64e0a1b814701f6
kjkunte <kjkunte@gmail.com>
2024-07-17 14:07:20 +0530

    for loop, ascii

---
+++ data_types/src/dtype4.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let mut sum: i32 = 0;
+    for i in -3..2{
+        sum += i
+    }
+    assert!(sum == -5);
+    for c in 'a'..='z' {
+        println!("{}", c as u8) // Printing charcters as decimals
+    }
+}

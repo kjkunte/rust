7d7a4208b476ad6a2ffaeaf842723228211f74c7
kjkunte <kjkunte@gmail.com>
2024-07-23 23:41:32 +0530

    tuples and shadowbinding

---
+++ rustInHalfnHr/src/shadowbinding.rs
@@ -0,0 +1,5 @@
+fn main(){
+    let x = 13;
+    let x = x + 3; // thi value of x will be considered no the previous one
+    println!("{}", x);
+}
---
+++ rustInHalfnHr/src/tuples.rs
@@ -0,0 +1,12 @@
+fn main(){
+    //Rust has tuples, which you can think of as "fixed-length collections of values of different types"
+    let pair = ('a', 17); // here this tuple has a character and a number together, ie a collection of different types
+    // let pair: (char, i32) = ('a', 17); // anotate
+
+    println!("{}", type_of(&pair));
+    fn type_of<T>(_ : &T) -> String {
+        format!("{}", std::any::type_name::<T>())
+    }
+    println!("{}", pair.0);
+    println!("{}", pair.1);
+}

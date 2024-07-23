8589a9918f4b822c320b91116309de9fa54f8cb9
kjkunte <kjkunte@gmail.com>
2024-07-23 22:17:51 +0530

    <T> Floating point format -- macro

---
+++ fp1.rs
@@ -0,0 +1,9 @@
+fn main(){
+    let x: i32 = 5;
+    assert_eq!("u32".to_string(), type_of(&x));
+    println!("Success!");
+}
+
+fn type_of<T>(_ : &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
